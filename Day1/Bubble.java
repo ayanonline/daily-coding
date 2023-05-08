ad4a64a4512377581a474722e0f4b04fc2e13ae5
ayan <ayanghosh1389@gmail.com>
2023-05-08 15:39:38 +0530

    basic star pattern

--- Day1/Bubble.java
+++ Day1/Bubble.java
@@ -1,4 +1,4 @@
-package sorting;
+package Day1;
 
 /**
  * Bubble
---
+++ Day2/Pattern.java
@@ -0,0 +1,13 @@
+package pattern;
+
+public class Pattern {
+    public static void main(String[] args) {
+        int a = 4;
+        for (int i = 0; i < a; i++) {
+            for (int j = 0; j < a - i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
