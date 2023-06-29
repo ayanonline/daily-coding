3e50b5d7d1e8a24ac00d827de8e2ae89df48e2bb
ayan <ayanghosh1389@gmail.com>
2023-06-29 16:10:53 +0530

    print subArray & find the minimum & maximum sum of all subarray

---
+++ Day9/SubArray.java
@@ -0,0 +1,34 @@
+public class SubArray {
+    public static void printSubArray(int arr[]) {
+        int ts = 0;
+        int min = Integer.MAX_VALUE;
+        int max = Integer.MIN_VALUE;
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = i; j < arr.length; j++) {
+                int sum = 0;
+                for (int k = i; k <= j; k++) {
+                    sum += arr[k];
+                    System.out.print(arr[k] + " "); // subarray
+                }
+                ts++;
+                System.out.println();
+                System.out.println("sum of this subArray is = " + sum);
+
+                if (sum > max) {
+                    max = sum;
+                }
+                if (sum < min) {
+                    min = sum;
+                }
+            }
+            System.out.println();
+        }
+        System.out.println("Total subarray = " + ts);
+        System.out.println("min = " + min + " and max = " + max);
+    }
+
+    public static void main(String[] args) {
+        int myArr[] = { 2, 4, 6, 8, 10 };
+        printSubArray(myArr);
+    }
+}
