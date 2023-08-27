b0687907c18d0f995be6a5efafd514259c546df5
ayan <ayanghosh1389@gmail.com>
2023-08-28 03:41:08 +0530

    diagonal sum problem solved

--- Day23/SpiralMatrix.java
+++ Day23/SpiralMatrix.java
@@ -37,13 +37,41 @@ class SpiralMatrix {
         }
     }
 
+    public static int diagonalSum(int arr[][]) {
+        int sum = 0;
+        int n = arr.length;
+        // for (int i = 0; i < arr.length; i++) { // O(n^2)
+        // // Primary diagonal sum
+        // for (int j = 0; j < arr.length; j++) {
+        // if (i == j) {
+        // sum += arr[i][j];
+        // }
+        // // secondary diagonal sum
+        // else if (i + j == arr.length - 1) {
+        // sum += arr[i][j];
+        // }
+        // }
+        // }
+
+        for (int i = 0; i < n; i++) {
+            // Primary diagonal
+            sum += arr[i][i];
+            // Secondary diagonal i+j=n-1 -> j=n-1-i
+            int j = n - 1 - i;
+            if (i != j)
+                sum += arr[i][j];
+
+        }
+        return sum;
+    }
+
     public static void main(String[] args) {
         int matrix[][] = {
-                { 1, 2, 3, 4 },
-                { 5, 6, 7, 8 },
-                { 9, 10, 11, 12 },
-                { 13, 14, 15, 16 }
+                { 1, 2, 3 },
+                { 5, 6, 7, },
+                { 9, 10, 11 }
         };
-        printSpiral(matrix);
+        // printSpiral(matrix);
+        System.out.println(diagonalSum(matrix));
     }
 }
